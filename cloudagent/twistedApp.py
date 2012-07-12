8c3ce87fa0fddbdbbc239e1b9f225f5531300530
Theo Julienne <theo@icy.com.au>
2012-07-12 12:11:38 +1000

    Add twisted service polling

--- cloudagent/twistedApp.py
+++ cloudagent/twistedApp.py
@@ -1,10 +1,35 @@
 from twisted.application import service
 from twisted.application.internet import TimerService
 
-def foo( ):
-	print 'poll'
+class MetricService(TimerService):
+	def __init__( self, metricProvider ):
+		super(MetricService, self).__init__( metricProvider.getInterval( ), self.processMetrics )
+		self.metricProvider = metricProvider
+	
+	def processMetrics( self ):
+		metrics = self.metricProvider.getMetrics( )
+		if metrics is not None:
+			if 'time' not in metrics:
+				metrics['time'] = time.time()
+			print json.dumps( metrics )
 
 application = service.Application( 'cloudagent' )
 
-timerService = TimerService( 1, foo )
-timerService.setServiceParent( application )
+# find the metrics
+modules = [
+	'Memory',
+	'Network'
+]
+
+def _getMetricProviders( ):
+	for module in modules:
+		className = module + 'Metric'
+		metricModule = __import__( 'metrics.' + className, globals(), locals(), [className] )
+		metricClass = getattr( metricModule, className )
+		metricInst = metricClass( )
+		yield metricInst
+
+# add the metric providers to the application as services
+for metricProvider in _getMetricProviders( ):
+	metricService = MetricService( metricProvider )
+	metricService.setServiceParent( application )
