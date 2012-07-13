1b6d7af7daf408d1723c425331d3bcbdf37040be
Theo Julienne <theo@icy.com.au>
2012-07-13 13:07:56 +1000

    Push server sketchings

---
+++ cloudagent/pushService.py
@@ -0,0 +1,47 @@
+from twisted.internet import reactor
+from twisted.internet import defer
+from twisted.web import client
+from twisted.web.http_headers import Headers
+
+import agent_settings
+
+def finished(bytes):
+	print 'Upload done', bytes
+
+def error(error):
+	print 'Upload error', error
+
+def progress( current, total ):
+	print 'Upload progress', current, 'out of', total
+
+def responseReady( response ):
+	print 'got response', response
+
+def doPush( filename ):
+	producerDeferred = defer.Deferred()
+	producerDeferred.addCallback(finished)
+	producerDeferred.addErrback(error)
+	
+	
+	multiPartProducer = MultiPartProducer(
+		files={
+			"upload": "/home/mariano/myfile.tar.gz"
+		},
+		data={
+			"field1": "value1"
+		},
+		callback=progress,
+		deferred=producerDeferred
+	)
+	
+	headers = Headers()
+	headers.addRawHeader( 'Content-Type', 'multipart/form-data; boundary=%s' % multiPart.boundary )
+	
+	agent = client.Agent( reactor )
+	request = agent.request(
+		'POST',
+		agent_settings.PUSH_DETAILS['url'],
+		headers,
+		multiPartProducer
+	)
+	request.addCallback( responseReady )
